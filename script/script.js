d527db02b7c5b0de731f4d55c3a1909e3b14ea49
agryz01 <agryz@yandex.ru>
2022-05-12 20:19:45 +0300

    на вторую итерацию

--- script/script.js
+++ script/script.js
@@ -1,31 +1,31 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closingIcon = document.querySelector('.popup__closing-icon');
-const button = document.querySelector('.popup__button');
+const formElement = popup.querySelector('.popup__container');
+const nameInput = popup.querySelector('.popup__input-text_input_name');
+const jobInput = popup.querySelector('.popup__input-text_input_activity');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
-editButton.addEventListener('click', function (event) {
-  console.log('нажатие кнопки edit');
-  popup.classList.add('popup__opened');
-});
-closingIcon.addEventListener('click', function (event) {
-  console.log('нажатие крестика');
-  popup.classList.remove('popup__opened');
+nameInput.value = profileTitle.textContent;
+jobInput.value = profileSubtitle.textContent;
+
+editButton.addEventListener('click', function() {
+  popup.classList.add('popup_opened');
 });
 
-let formElement = popup.querySelector('.popup__container');
-let nameInput = popup.querySelector('.popup__input-text_input_name');
-let jobInput = popup.querySelector('.popup__input-text_input_activity');
+function closeForm() {
+  popup.classList.remove('popup_opened');
+}
+
+closingIcon.addEventListener('click', closeForm);
 
 function formSubmitHandler(evt) {
-  console.log('нажатие кнопки "Сохранить"');
   evt.preventDefault();
-
-  let profileTitle = document.querySelector('.profile__title');
-  let profileSubtitle = document.querySelector('.profile__subtitle');
-
 
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  popup.classList.remove('popup__opened');
+  closeForm();
 }
+
 formElement.addEventListener('submit', formSubmitHandler);
